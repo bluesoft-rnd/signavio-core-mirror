ca791146319f7b3fcf9254dcaf9ab70f76ce1dd3
nicolas.peters@signavio.com <nicolas.peters@signavio.com@ed087902-8a0d-0a71-2a74-18b522bb7755>
2010-09-22 08:30:33 +0000

    Preparing Java 5 Compatibility: Diagram API
    git-svn-id: http://signavio-core-components.googlecode.com/svn/trunk@22 ed087902-8a0d-0a71-2a74-18b522bb7755

--- platform extensions/diagram/src/org/oryxeditor/server/diagram/Shape.java
+++ platform extensions/diagram/src/org/oryxeditor/server/diagram/Shape.java
@@ -277,7 +277,7 @@ public class Shape implements Stencil, Bounded{
 	/* (non-Javadoc)
 	 * @see org.oryxeditor.server.diagram.Bounded#height()
 	 */
-	@Override
+//	@Override
 	public double getHeight() {
 		return this.getLowerRight().getY() - this.getUpperLeft().getY();
 	}
@@ -285,7 +285,7 @@ public class Shape implements Stencil, Bounded{
 	/* (non-Javadoc)
 	 * @see org.oryxeditor.server.diagram.Bounded#width()
 	 */
-	@Override
+//	@Override
 	public double getWidth() {
 		return this.getLowerRight().getX() - this.getUpperLeft().getX();
 	}
