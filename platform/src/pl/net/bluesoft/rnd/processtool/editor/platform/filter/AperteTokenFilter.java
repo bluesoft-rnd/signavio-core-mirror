30f30a55666314cc59159f94bf9f7b8ca51936ff
lbortlik <lbortlik@bluesoft.net.pl>
2012-01-19 22:09:34 +0100

    security: code cleaning

--- platform/src/pl/net/bluesoft/rnd/processtool/editor/platform/filter/AperteTokenFilter.java
+++ platform/src/pl/net/bluesoft/rnd/processtool/editor/platform/filter/AperteTokenFilter.java
@@ -1,12 +1,9 @@
 package pl.net.bluesoft.rnd.processtool.editor.platform.filter;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -21,7 +18,6 @@ import com.signavio.platform.core.PlatformProperties;
 public class AperteTokenFilter implements Filter {
 
     private static final String APERTE_TOKEN_ATTRIBUTE_NAME = "aperteToken";
-    private static final Logger logger = Logger.getLogger(AperteTokenFilter.class);
     
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -74,23 +70,5 @@ public class AperteTokenFilter implements Filter {
        // destroy http client
     }
 
-    private String getAperteToken() {
-    	 PlatformProperties props = Platform.getInstance().getPlatformProperties();
-     	 String tokenUrl = props.getServerName() + props.getJbpmGuiUrl() + "/token?9d20dc34-6f15-4650-a8e6-cb6292e7a729=8ab24b1f-1229-34fe-ae94-a359b31e821c";
-         try {
-         	URL url = new URL(tokenUrl);
- 	        URLConnection conn = url.openConnection();
- 	        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
- 	        StringBuffer sb = new StringBuffer();
- 	        String line;
- 	        while ((line = rd.readLine()) != null) {
- 	            sb.append(line);
- 	        }
- 	        rd.close();
- 	        return sb.toString();
-         } catch (IOException e) {
-         	logger.error("Error reading data from " + tokenUrl, e);
-         	return null;
-         }
-    }
+   
 }
